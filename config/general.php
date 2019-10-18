473932db7fb8ec35ac1f65fbdc68e079835e5d85
Mischa Sprecher <nibiru@mac.com>
2019-10-18 14:45:43 +0200

    local url changed. merged step-12 into start

--- config/general.php
+++ config/general.php
@@ -30,7 +30,7 @@ return [
     // Dev environment settings
     'dev' => [
         // Base site URL
-        'siteUrl' => ontherocks.test,
+        'siteUrl' => 'https://ontherocks.localhost',
 
         // Dev Mode (see https://craftcms.com/support/dev-mode)
         'devMode' => true,
@@ -45,6 +45,6 @@ return [
     // Production environment settings
     'production' => [
         // Base site URL
-        'siteUrl' => 'http://barkeeper.parhelia.ch',
+        'siteUrl' => 'https://barkeeper.parhelia.ch',
     ],
 ];
